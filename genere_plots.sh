499073d6993be024b00ed1d044067f5cc0720ea2
Thibaut Marcq <50046548+thibautmarcq@users.noreply.github.com>
2024-11-17 15:26:07 +0000

    Modification des légendes et titres des graphs

--- genere_plots.sh
+++ genere_plots.sh
@@ -9,30 +9,30 @@ for file in results/*.txt; do
     gnuplot -e "
         set terminal png;
         set output 'graphs/${base_name}_plot.png';
-        set title 'Algorithm Times';
-        set xlabel 's';
-        set ylabel 'Time (seconds)';
+        set title '${base_name}';
+        set xlabel 'Valeur de s';
+        set ylabel 'Temps (secondes)';
         set key inside;
         set xtics rotate by -45;
         set format x '%.0f';
-        plot '$file' using 1:2 with points title 'Algo1 Time', \
-             '$file' using 1:3 with points title 'Algo2 Time', \
-             '$file' using 1:4 with points title 'Algo3 Time';
+        plot '$file' using 1:2 with points title 'Temps AlgoI', \
+             '$file' using 1:3 with points title 'Temps AlgoII', \
+             '$file' using 1:4 with points title 'Temps AlgoIII';
     "
     
     # Générer le graphique avec les valeurs de s comprises entre 0 et 5000
     gnuplot -e "
         set terminal png;
         set output 'graphs/${base_name}_plot_0_1000.png';
-        set title 'Algorithm Times (s between 0 and 1000)';
-        set xlabel 's';
-        set ylabel 'Time (seconds)';
+        set title '${base_name} (entre 0 et 1000)';
+        set xlabel 'Valeur de s';
+        set ylabel 'Temps (secondes)';
         set key inside;
         set xtics rotate by -45;
         set xrange [0:1000];
         set format x '%.0f';
-        plot '$file' using 1:2 with points title 'Algo1 Time', \
-             '$file' using 1:3 with points title 'Algo2 Time', \
-             '$file' using 1:4 with points title 'Algo3 Time';
+        plot '$file' using 1:2 with points title 'Temps AlgoI', \
+             '$file' using 1:3 with points title 'Temps AlgoII', \
+             '$file' using 1:4 with points title 'Temps AlgoIII';
     "
 done
